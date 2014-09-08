2176b654ee20baad0d671f70a474ecb166b2daed
bubbathetroll <cai.alvan8@gmail.com>
2014-09-08 12:27:51 -0700

    Added first name to index.html

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,36 @@
+
+
 void setup()
 {
   size(300,300);
+  strokeWeight(10);
+  background(0);
+  noLoop();
 }
-void draw()
-{
+void draw(){
+	int startX, startY, endX, endY;
+	startX = 0;
+	startY = 150;
+	endX = 0;
+	endY = 0;
+
+	float newRandom1 = (int)(Math.random()*256);
+	float newRandom2 = (int)(Math.random()*256);
+	float newRandom3 = (int)(Math.random()*256);
+	stroke(newRandom1, newRandom2, newRandom3);
+	while(endX < 300){
+		endX = startX + (int)(Math.random()*9);
+		endY = startY+ (int)((Math.random()*18)-9);
+		line(startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+	}
+
+
 
 }
 void mousePressed()
 {
-
+	redraw();
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,40 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+
+
 public void setup()
 {
   size(300,300);
+  strokeWeight(10);
+  background(0);
   noLoop();
 }
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+public void draw(){
+	int startX, startY, endX, endY;
+	startX = 0;
+	startY = 150;
+	endX = 0;
+	endY = 0;
+
+	float newRandom1 = (int)(Math.random()*256);
+	float newRandom2 = (int)(Math.random()*256);
+	float newRandom3 = (int)(Math.random()*256);
+	stroke(newRandom1, newRandom2, newRandom3);
+	while(endX < 300){
+		endX = startX + (int)(Math.random()*9);
+		endY = startY+ (int)((Math.random()*18)-9);
+		line(startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+	}
+
+
+
 }
 public void mousePressed()
 {
-  redraw();
+	redraw();
 }
 
   static public void main(String[] passedArgs) {
